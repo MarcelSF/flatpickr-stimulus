328730eea62c8d8b21b1ed5c930842256c0811d1
Marcel Fonseca <marcel@lewagon.org>
2022-11-10 18:29:42 +0100

    Properly target and use Stimulus

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,21 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
-  static targets = [ "end-date" ]
-
+  static targets = [ "startTime", "endTime" ]
   connect() {
-    // require("flatpickr/dist/themes/dark.css");
-    // console.log('FlatpickrController#connect')
-    const options = {
-      mode: "range",
-      enableTime: true,
-      // altInput: true,
-      minDate: "today",
-      plugins: [new rangePlugin({ input: "#range_end"})]
-    }
-    flatpickr(this.element, options)
+    flatpickr(this.startTimeTarget, { altInput: true })
+    flatpickr(this.endTimeTarget, { altInput: true })
   }
 }
